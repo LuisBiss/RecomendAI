bd4b536e3fc728bd588d5606cc052b2c5dec18f5
Luis Francisco Bissoli <114625678+LuisBiss@users.noreply.github.com>
2025-04-23 15:53:08 -0300

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -3,7 +3,6 @@ import openai
 import json
 import numpy as np
 
-client = openai.OpenAI(base_url="http://192.168.143.122:1234/v1", api_key="not-needed")
 amostra = {
     "Colesterol": {"valor": np.random.uniform(110, 260)},
     "Glicose": {"valor": np.random.uniform(80, 220)},
@@ -12,38 +11,74 @@ amostra = {
     "TSH": {"valor": np.random.uniform(0.3, 14.0)},
     "Triglicerídeos": {"valor": np.random.uniform(80, 700)},
 }
-diet = client.responses.create(
-    model="local-model",
-    input=[
-        {"role": "system", "content": "You are a helpful nutricionist. With the follow blood sample make a diet schedule."},
-        {"role": "user", "content":  json.dumps(amostra, indent=4, ensure_ascii=False)}
-    ],
-    text={
-        "format": {
-		 "type": "json_schema",
-            "name": "diet",
-            "schema": {
-			"type": "object",
-                "properties": {
-                    "days": {
-					"type": "array",
-                        "items": {
-                            "type": "object",
-                            "properties": {
-                                "time": { "type": "string" },
-                                "food": { "type": "string" }
-							},
-                            "required": ["time", "food"],
-                            "additionalProperties": False
-						}
-					},
-					"final_answer": { "type": "string" }
-				},
-                "required": ["days"],
-				 "additionalProperties": False
-			},
-			"strict": True
-		}
-	}
+# print(json.dumps(amostra, indent=4, ensure_ascii=False))
+
+# Conexão no lm studio
+client = openai.OpenAI(
+    base_url="http://localhost:1234/v1",
+    api_key="lm-studio"
 )
-event = json.loads(diet.output_text)
+
+# setup das menssagens para o modelo
+messages = [
+    {"role": "system", "content": "Você é um nutricionista. Faça uma rotina de dieta com os dados referente de amostras de sangue."},
+    {"role": "user", "content":  json.dumps(amostra, indent=4, ensure_ascii=False)}
+]
+
+#Setup do formato da saida
+character_schema = {
+    "type": "json_schema",
+    "name": "diet",
+    "schema": {
+    "type": "object",
+        "properties": {
+            "days": {
+            "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "time": { "type": "string" },
+                        "food": { "type": "string" }
+                    },
+                    "required": ["time", "food"],
+                }
+            }
+        },
+        "required": ["days"],
+    }
+}
+
+# Resposta do LM
+response = client.chat.completions.create(
+    model="meta-llama-3.1-8b-instruct",
+    messages=messages,
+    temperature=0.7,
+    max_tokens=10000,
+    response_format= {
+    "type" : "json_schema",
+    "json_schema": {
+        "type": "json_schema",
+        "name": "diet",
+        "schema": {
+            "type": "object",
+            "properties": {
+            "diet": {
+            "type": "array",
+            "items": {
+            "type": "object",
+            "properties": {
+            "day": { "type": "string" },
+            "routine": { "type": "array", "items": {"type":"object","properties":{"time": { "type": "string" },
+            "food": { "type": "string" }}} }
+            },
+            "required": ["time", "food"],
+            }
+            }
+            },
+            "required": ["days"],
+        }
+    }
+    }
+        
+)
+print(response.choices[0].message.content)
